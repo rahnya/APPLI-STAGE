e4c515bec29f5709ea133ca37eea11155a89a8bd
mimiidjd <147587594+mimiidjd@users.noreply.github.com>
2025-01-22 17:15:49 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,14 @@
 <?php
-try {
-    $bdd = new PDO('mysql:host=localhost;dbname=stage;charset=utf8mb4', 'root', '');
-    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Erreur de connexion : " . $e->getMessage());
-}
+// Connexion à la bdd :
+$bd = new PDO('mysql:host=localhost;dbname=stage;charset=utf8mb4', 'root', '');
+    global $bdd;
+
+    try {
+        $bdd = 
+        $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $bdd->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    } catch (PDOException $e) {
+        die("Erreur de connexion à la base de données : " . $e->getMessage());
+    }
 ?>
+
