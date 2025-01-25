b5944be464b34b10c17f6c54004a6802f33c3623
Rahnya <151737871+rahnya@users.noreply.github.com>
2025-01-25 02:57:14 +0100

    suppression de db.php car il y a déjà config.php

--- db.php
+++
@@ -1,8 +0,0 @@
-<?php
-try {
-    $pdo = new PDO('mysql:host=localhost;dbname=stagefinal;charset=utf8mb4', 'root', '');
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Erreur de connexion à la base de données : " . $e->getMessage());
-}
-?>
