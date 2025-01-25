904aace5581d8bf7c3112a0f9f7ac1417f5dc706
Rahnya <151737871+rahnya@users.noreply.github.com>
2025-01-25 04:25:18 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,13 +1,9 @@
 <?php
-
     global $bdd;
-
-    try {
-        $bdd = new PDO('mysql:host=localhost;dbname=bdd_stage;charset=utf8mb4', 'root', '');
-        $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $bdd->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-    } catch (PDOException $e) {
-        die("Erreur de connexion à la base de données : " . $e->getMessage());
-    }
+try {
+    $pdo = new PDO('mysql:host=localhost;dbname=bdd_stage;charset=utf8mb4', 'root', '');
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Erreur de connexion à la base de données : " . $e->getMessage());
+}
 ?>
-
