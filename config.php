75efe676749c31f7485ad9f21df3d31a88161abb
Rahnya <151737871+rahnya@users.noreply.github.com>
2025-01-25 02:51:23 +0100

    Update config.php

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
     global $bdd;
 
     try {
-        $bdd = new PDO('mysql:host=localhost;dbname=stage;charset=utf8mb4', 'root', '');
+        $bdd = new PDO('mysql:host=localhost;dbname=bdd_stage;charset=utf8mb4', 'root', '');
         $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $bdd->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     } catch (PDOException $e) {
