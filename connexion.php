c6d34fa38681c864a0a814769ab767a2555c681e
Rahnya <151737871+rahnya@users.noreply.github.com>
2025-01-25 02:07:41 +0100

    controleur de la page connexion

--- connexion.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once 'UserController.php';
-
-$controller = new UserController();
-$controller->seConnecter();
-
-require 'index.html';
---
+++ connexionControleur.php
@@ -0,0 +1,24 @@
+<?php
+// pour gérer la connexion
+session_start();
+require_once 'connexionModele.php';
+
+$error = '';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['login'])) {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $user = authenticate($email, $password);
+
+    if ($user) {
+        $_SESSION['user'] = $user;
+        header('Location: index.php');
+        exit;
+    } else {
+        $error = 'Identifiants incorrects.';
+    }
+}
+
+// Pour afficher la page avec le formulaire à nouveau
+require_once 'index.php';
+?>
